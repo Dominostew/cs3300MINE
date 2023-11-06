f630383c97a874b4a6e40c573b2ca74c03335f75
Dominostew <dominostew@gmail.com>
2023-11-05 19:39:47 -0700

    setting changes

--- my_project/settings.py
+++ my_project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #add app name
+    'project_app",
+]
+
+#authenticating users
+AUTHENTICATION_BACKENDS = [
+     'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
