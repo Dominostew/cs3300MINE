1dc1b17afcbaa9a9656283b0445bf533208e58b2
Dominostew <dominostew@gmail.com>
2023-11-26 21:21:59 -0700

    calendartroubles

--- project_app/views.py
+++ project_app/views.py
@@ -8,7 +8,3 @@ def home(request):
 
 def user(request):
      return render(request, 'project_app/user.html')
-
-def calendar(request):
-     calendar = Calendar.objects.all()
-     return render(request, 'project_app/calendar.html', {'calendar':calendar})
